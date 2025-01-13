1f995d3cf357de0935b9b304c12e97cc7c90e909
Vladimir Kul'kov <kulkov.vk@dns-shop.ru>
2025-01-13 23:38:12 +1000

    chore: add more logging

--- src/database.py
+++ src/database.py
@@ -14,11 +14,13 @@ class Database:
 
     async def connect(self) -> None:
         self.pool = await asyncpg.create_pool(dsn=self.dsn)
+        logger.debug("Created database pool")
 
     async def create_tables(self) -> None:
         async with self.pool.acquire() as connection:
             await self._create_habr_career_user_table(connection)
             await self._create_users_to_check_table(connection)
+        logger.debug("Created database tables")
 
     async def _create_habr_career_user_table(
         self, connection: PoolConnectionProxy
@@ -34,6 +36,8 @@ class Database:
             qualification VARCHAR(255) NOT NULL
         )
         """
+        await connection.execute(query)
+        logger.debug("Created habr_career_users table")
         index = """
         CREATE UNIQUE INDEX IF NOT EXISTS unique_user_fields_nullable
         ON habr_career_users (
@@ -45,8 +49,8 @@ class Database:
             COALESCE(last_visited, last_visited)
         );
         """
-        await connection.execute(query)
         await connection.execute(index)
+        logger.debug("Created habr_career_users unique index")
 
     async def _create_users_to_check_table(
         self, connection: PoolConnectionProxy
@@ -57,6 +61,7 @@ class Database:
         )
         """
         await connection.execute(query)
+        logger.debug("Created users_to_check table")
 
     async def insert_user(self, user: HabrCareerUser) -> None:
         query = (
